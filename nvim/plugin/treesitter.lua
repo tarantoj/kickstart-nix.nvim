e853f874de85cc912a4b212cd87178d1b3e195ac
James Taranto <taranto.james@gmail.com>
2024-03-10 13:41:41 +1100

    remove duplicate commentstring setup

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -107,8 +107,6 @@ require('treesitter-context').setup {
   max_lines = 3,
 }
 
-require('ts_context_commentstring').setup()
-
 -- Tree-sitter based folding
 -- vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'

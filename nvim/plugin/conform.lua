8dbb8f6e0c3c884abc799acffaab8594c8813aec
James Taranto <taranto.james@gmail.com>
2024-03-11 20:52:49 +1100

    add resharper as a formatter

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -6,10 +6,19 @@ require('conform').setup {
     -- Use a sub-list to run only the first available formatter
     javascript = { { 'prettierd', 'prettier' } },
     nix = { 'alejandra' },
+    csharp = { 'resharper' },
   },
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
+  },
+  formatters = {
+    resharper = {
+      args = { 'cleanupcode', '$FILENAME' },
+      command = 'jb',
+      stdin = false,
+      cwd = require('conform.util').root_file { '.git', '*.sln' },
+    },
   },
 }

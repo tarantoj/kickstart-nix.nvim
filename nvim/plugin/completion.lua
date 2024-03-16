aabfa5d5a452d771db7bd84b6b80ccfaed6035ba
James Taranto <taranto.james@gmail.com>
2024-03-16 20:14:15 +1100

    remove keyword limit for completion

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -126,8 +126,12 @@ cmp.setup {
   sources = cmp.config.sources {
     -- The insertion order influences the priority of the sources
     { name = 'luasnip' },
-    { name = 'nvim_lsp', keyword_length = 3 },
-    { name = 'nvim_lsp_signature_help', keyword_length = 3 },
+    {
+      name = 'nvim_lsp', --[[ keyword_length = 3 ]]
+    },
+    {
+      name = 'nvim_lsp_signature_help', --[[ keyword_length = 3 ]]
+    },
     { name = 'buffer' },
     { name = 'path' },
   },

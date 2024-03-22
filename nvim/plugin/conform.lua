23d5711d39d1cdada69cb4f9a7d5de9a98276ae3
James Taranto <james.taranto@mudbath.com.au>
2024-03-22 11:30:55 +1100

    disable resharper in conform

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -4,7 +4,7 @@ require('conform').setup {
     -- Conform will run multiple formatters sequentially
     -- python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
-    csharp = { 'resharper' },
+    -- csharp = { 'resharper' },
     css = { { 'prettierd', 'prettier' } },
     html = { { 'prettierd', 'prettier' } },
     javascript = { { 'prettierd', 'prettier' } },

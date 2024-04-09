a7031c8d156bbe7272f8e967e8131b9de55204ae
James Taranto <james.taranto@mudbath.com.au>
2024-04-09 12:02:39 +1000

    add neogen keymap

--- nvim/plugin/comment.lua
+++ nvim/plugin/comment.lua
@@ -7,3 +7,5 @@ require('Comment').setup {
 }
 
 require('neogen').setup { snippet_engine = 'luasnip', languages = { cs = { template = { annotation_convention = 'xmldoc' } } } }
+
+vim.keymap.set('n', '<Leader>nf', require('neogen').generate, { desc = '[neogen] generate' })

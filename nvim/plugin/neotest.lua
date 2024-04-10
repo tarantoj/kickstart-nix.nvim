6305d597acaa4e785269f9fb5e12df951a13b188
James Taranto <taranto.james@gmail.com>
2024-04-11 00:24:59 +1000

    neotest keybinds

--- nvim/plugin/neotest.lua
+++ nvim/plugin/neotest.lua
@@ -4,3 +4,33 @@ require('neotest').setup {
     require 'neotest-dotnet',
   },
 }
+
+local keymap = vim.keymap
+
+-- keymap.set('n', "<leader>tt",
+keymap.set('n', '<leader>tt', function()
+  require('neotest').run.run(vim.fn.expand '%')
+end, { desc = 'Run File' })
+keymap.set('n', '<leader>tT', function()
+  require('neotest').run.run(vim.uv.cwd())
+end, { desc = 'Run All Test Files' })
+keymap.set('n', '<leader>tr', function()
+  require('neotest').run.run()
+end, { desc = 'Run Nearest' })
+keymap.set('n', '<leader>tl', function()
+  require('neotest').run.run_last()
+end, { desc = 'Run Last' })
+keymap.set('n', '<leader>ts', function()
+  require('neotest').summary.toggle()
+end, { desc = 'Toggle Summary' })
+keymap.set('n', '<leader>to', function()
+  require('neotest').output.open { enter = true, auto_close = true }
+end, { desc = 'Show Output' })
+keymap.set('n', '<leader>tO', function()
+  require('neotest').output_panel.toggle()
+end, { desc = 'Toggle Output Panel' })
+keymap.set('n', '<leader>tS', function()
+  require('neotest').run.stop()
+end, { desc = 'Stop' })
+
+require('which-key').register { ['<leader>t'] = { name = '+test' } }

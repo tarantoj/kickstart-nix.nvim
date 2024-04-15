d7d2f17d68fe1b9e96b8a897ef9af35e544792df
James Taranto <taranto.james@gmail.com>
2024-04-16 00:03:51 +1000

    formatting

--- nvim/plugin/comment.lua
+++ nvim/plugin/comment.lua
@@ -1,3 +1,8 @@
-require('neogen').setup { snippet_engine = 'luasnip', languages = { cs = { template = { annotation_convention = 'xmldoc' } } } }
+require('neogen').setup {
+  snippet_engine = 'luasnip',
+  languages = {
+    cs = { template = { annotation_convention = 'xmldoc' } },
+  },
+}
 
 vim.keymap.set('n', '<Leader>ng', require('neogen').generate, { desc = '[neogen] generate' })

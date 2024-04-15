cb418c17bd941b30b49cebdb645775abd2cd3c0d
James Taranto <taranto.james@gmail.com>
2024-04-16 00:03:44 +1000

    change telescope mappings

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,137 +1,58 @@
-if vim.g.did_load_telescope_plugin then
-  return
-end
-vim.g.did_load_telescope_plugin = true
-
 local telescope = require 'telescope'
 local actions = require 'telescope.actions'
 
 local builtin = require 'telescope.builtin'
+local keymap = vim.keymap
 
-local layout_config = {
-  vertical = {
-    width = function(_, max_columns)
-      return math.floor(max_columns * 0.99)
-    end,
-    height = function(_, _, max_lines)
-      return math.floor(max_lines * 0.99)
-    end,
-    prompt_position = 'bottom',
-    preview_cutoff = 0,
-  },
-}
-
--- Fall back to find_files if not in a git repo
-local project_files = function()
-  local opts = {} -- define here if you want to define something
-  local ok = pcall(builtin.git_files, opts)
-  if not ok then
-    builtin.find_files(opts)
-  end
-end
-
----@param picker function the telescope picker to use
-local function grep_current_file_type(picker)
-  local current_file_ext = vim.fn.expand '%:e'
-  local additional_vimgrep_arguments = {}
-  if current_file_ext ~= '' then
-    additional_vimgrep_arguments = {
-      '--type',
-      current_file_ext,
-    }
-  end
-  local conf = require('telescope.config').values
-  picker {
-    vimgrep_arguments = vim.tbl_flatten {
-      conf.vimgrep_arguments,
-      additional_vimgrep_arguments,
-    },
-  }
-end
-
---- Grep the string under the cursor, filtering for the current file type
-local function grep_string_current_file_type()
-  grep_current_file_type(builtin.grep_string)
-end
-
---- Live grep, filtering for the current file type
-local function live_grep_current_file_type()
-  grep_current_file_type(builtin.live_grep)
-end
-
---- Like live_grep, but fuzzy (and slower)
-local function fuzzy_grep(opts)
-  opts = vim.tbl_extend('error', opts or {}, { search = '', prompt_title = 'Fuzzy grep' })
-  builtin.grep_string(opts)
-end
-
-local function fuzzy_grep_current_file_type()
-  grep_current_file_type(fuzzy_grep)
-end
-
-vim.keymap.set('n', '<leader>tp', function()
-  builtin.find_files()
-end, { desc = '[t]elescope find files - ctrl[p] style' })
-vim.keymap.set('n', '<M-p>', builtin.oldfiles, { desc = '[telescope] old files' })
-vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = '[telescope] live grep' })
-vim.keymap.set('n', '<leader>tf', fuzzy_grep, { desc = '[t]elescope [f]uzzy grep' })
-vim.keymap.set('n', '<M-f>', fuzzy_grep_current_file_type, { desc = '[telescope] fuzzy grep filetype' })
-vim.keymap.set('n', '<M-g>', live_grep_current_file_type, { desc = '[telescope] live grep filetype' })
-vim.keymap.set('n', '<leader>t*', grep_string_current_file_type, { desc = '[t]elescope grep current string [*] in current filetype' })
-vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = '[telescope] grep current string [*]' })
-vim.keymap.set('n', '<leader>tg', project_files, { desc = '[t]elescope project files [g]' })
-vim.keymap.set('n', '<leader>tc', builtin.quickfix, { desc = '[t]elescope quickfix list [c]' })
-vim.keymap.set('n', '<leader>tq', builtin.command_history, { desc = '[t]elescope command history [q]' })
-vim.keymap.set('n', '<leader>tl', builtin.loclist, { desc = '[t]elescope [l]oclist' })
-vim.keymap.set('n', '<leader>tr', builtin.registers, { desc = '[t]elescope [r]egisters' })
-vim.keymap.set('n', '<leader>tbb', builtin.buffers, { desc = '[t]elescope [b]uffers [b]' })
-vim.keymap.set('n', '<leader>tbf', builtin.current_buffer_fuzzy_find, { desc = '[t]elescope current [b]uffer [f]uzzy find' })
-vim.keymap.set('n', '<leader>td', builtin.lsp_document_symbols, { desc = '[t]elescope lsp [d]ocument symbols' })
-vim.keymap.set('n', '<leader>to', builtin.lsp_dynamic_workspace_symbols, { desc = '[t]elescope lsp dynamic w[o]rkspace symbols' })
+keymap.set('n', '<leader>,', '<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>', { desc = 'Switch Buffer' })
+-- keymap.set('n', "<leader>/", LazyVim.telescope("live_grep"), desc = "Grep (Root Dir)" })
+keymap.set('n', '<leader>:', '<cmd>Telescope command_history<cr>', { desc = 'Command History' })
+-- keymap.set('n', "<leader><space>", LazyVim.telescope("files"), desc = "Find Files (Root Dir)" })
+-- find
+keymap.set('n', '<leader>fb', '<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>', { desc = 'Buffers' })
+-- keymap.set('n', "<leader>fc", LazyVim.telescope.config_files(), desc = "Find Config File" })
+keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+keymap.set('n', '<leader>fF', builtin.git_files, { desc = 'Find Files (git)' })
+keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find Files (git-files)' })
+keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<cr>', { desc = 'Recent' })
+-- keymap.set('n', "<leader>fR", LazyVim.telescope("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" })
+-- git
+keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<CR>', { desc = 'Commits' })
+keymap.set('n', '<leader>gs', '<cmd>Telescope git_status<CR>', { desc = 'Status' })
+-- search
+keymap.set('n', '<leader>s"', '<cmd>Telescope registers<cr>', { desc = 'Registers' })
+keymap.set('n', '<leader>sa', '<cmd>Telescope autocommands<cr>', { desc = 'Auto Commands' })
+keymap.set('n', '<leader>sb', '<cmd>Telescope current_buffer_fuzzy_find<cr>', { desc = 'Buffer' })
+keymap.set('n', '<leader>sc', '<cmd>Telescope command_history<cr>', { desc = 'Command History' })
+keymap.set('n', '<leader>sC', '<cmd>Telescope commands<cr>', { desc = 'Commands' })
+keymap.set('n', '<leader>sd', '<cmd>Telescope diagnostics bufnr=0<cr>', { desc = 'Document Diagnostics' })
+keymap.set('n', '<leader>sD', '<cmd>Telescope diagnostics<cr>', { desc = 'Workspace Diagnostics' })
+-- keymap.set('n', "<leader>sg", LazyVim.telescope("live_grep"), desc = "Grep (Root Dir)" })
+-- keymap.set('n', "<leader>sG", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" })
+keymap.set('n', '<leader>sh', '<cmd>Telescope help_tags<cr>', { desc = 'Help Pages' })
+keymap.set('n', '<leader>sH', '<cmd>Telescope highlights<cr>', { desc = 'Search Highlight Groups' })
+keymap.set('n', '<leader>sk', '<cmd>Telescope keymaps<cr>', { desc = 'Key Maps' })
+keymap.set('n', '<leader>sM', '<cmd>Telescope man_pages<cr>', { desc = 'Man Pages' })
+keymap.set('n', '<leader>sm', '<cmd>Telescope marks<cr>', { desc = 'Jump to Mark' })
+keymap.set('n', '<leader>so', '<cmd>Telescope vim_options<cr>', { desc = 'Options' })
+keymap.set('n', '<leader>sR', '<cmd>Telescope resume<cr>', { desc = 'Resume' })
+-- keymap.set('n', "<leader>sw", LazyVim.telescope("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" })
+-- keymap.set('n', "<leader>sW", LazyVim.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" })
+-- keymap.set('n', "<leader>sw", LazyVim.telescope("grep_string"), mode = "v", desc = "Selection (Root Dir)" })
+-- keymap.set('n', "<leader>sW", LazyVim.telescope("grep_string", { cwd = false }), mode = "v", desc = "Selection (cwd)" })
+-- keymap.set('n', "<leader>uC", LazyVim.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" })
+-- keymap.set('n', '<leader>ss', function()
+--   require('telescope.builtin').lsp_document_symbols {
+--     symbols = require('lazyvim.config').get_kind_filter(),
+--   }
+-- end, { desc = 'Goto Symbol' })
+-- keymap.set('n', '<leader>sS', function()
+--   require('telescope.builtin').lsp_dynamic_workspace_symbols {
+--     symbols = require('lazyvim.config').get_kind_filter(),
+--   }
+-- end, { desc = 'Goto Symbol (Workspace)' })
 
 telescope.setup {
-  defaults = {
-    path_display = {
-      'truncate',
-    },
-    layout_strategy = 'vertical',
-    layout_config = layout_config,
-    mappings = {
-      i = {
-        ['<C-q>'] = actions.send_to_qflist,
-        ['<C-l>'] = actions.send_to_loclist,
-        -- ['<esc>'] = actions.close,
-        ['<C-s>'] = actions.cycle_previewers_next,
-        ['<C-a>'] = actions.cycle_previewers_prev,
-      },
-      n = {
-        q = actions.close,
-      },
-    },
-    preview = {
-      treesitter = true,
-    },
-    history = {
-      path = vim.fn.stdpath 'data' .. '/telescope_history.sqlite3',
-      limit = 1000,
-    },
-    color_devicons = true,
-    set_env = { ['COLORTERM'] = 'truecolor' },
-    prompt_prefix = '   ',
-    selection_caret = '  ',
-    entry_prefix = '  ',
-    initial_mode = 'insert',
-    vimgrep_arguments = {
-      'rg',
-      '-L',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-    },
-  },
   extensions = {
     fzy_native = {
       override_generic_sorter = false,

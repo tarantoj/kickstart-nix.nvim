56a98d6c7401acaf99d4dabc0e3d25a8a360b42a
James Taranto <taranto.james@gmail.com>
2024-05-19 20:54:52 +1000

    disable neovim nightly for now

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          neovim-nightly-overlay.overlay
+          # neovim-nightly-overlay.overlay
           # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<nvim-pkg>
           neovim-overlay
           # This adds a function can be used to generate a .luarc.json

7bbcd131efc6e37a1cd30a365c1cb30a177618cc
James Taranto <taranto.james@gmail.com>
2024-06-20 18:46:08 +1000

    Revert "use bash language server from nodePackages"
    This reverts commit 65c22889d7cb7809a942e3e6c1183198b6acc0a2.

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -126,7 +126,7 @@ with final.pkgs.lib; let
   extraPackages = with pkgs; [
     # language servers, etc.
     lua-language-server
-    nodePackages.bash-language-server
+    bash-language-server
     lemminx
     dockerfile-language-server-nodejs
     docker-compose-language-service

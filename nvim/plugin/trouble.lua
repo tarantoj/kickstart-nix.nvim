84268ae74c8b12fe500e0b3966a4fc4720a71397
James Taranto <taranto.james@gmail.com>
2024-06-30 22:00:54 +1000

    fix trouble diagnostics and add open with telescope

--- nvim/plugin/trouble.lua
+++ nvim/plugin/trouble.lua
@@ -1,6 +1,6 @@
 -- Lua
 vim.keymap.set('n', '<leader>xx', function()
-  require('trouble').toggle()
+  require('trouble').toggle('diagnostics')
 end, { desc = 'problems' })
 vim.keymap.set('n', '<leader>xw', function()
   require('trouble').toggle('workspace_diagnostics')
@@ -17,3 +17,20 @@ end, { desc = 'loclist' })
 vim.keymap.set('n', 'gR', function()
   require('trouble').toggle('lsp_references')
 end, { desc = 'lsp references' })
+
+local actions = require('telescope.actions')
+local open_with_trouble = require('trouble.sources.telescope').open
+
+-- Use this to add more results without clearing the trouble list
+local add_to_trouble = require('trouble.sources.telescope').add
+
+local telescope = require('telescope')
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = { ['<c-t>'] = open_with_trouble },
+      n = { ['<c-t>'] = open_with_trouble },
+    },
+  },
+}

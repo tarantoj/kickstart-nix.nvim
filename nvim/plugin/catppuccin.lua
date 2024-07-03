480334c0b0120b74a40f29564d1d98a879039a6f
James Taranto <taranto.james@gmail.com>
2024-07-03 18:40:06 +1000

    enable catppuccin integrations

--- nvim/plugin/catppuccin.lua
+++ nvim/plugin/catppuccin.lua
@@ -3,7 +3,43 @@ if vim.g.did_load_catppuccin_plugin then
 end
 vim.g.did_load_catppuccin_plugin = true
 
-require('catppuccin').setup {}
+require('catppuccin').setup {
+  integrations = {
+    aerial = true,
+    alpha = true,
+    cmp = true,
+    dashboard = true,
+    flash = true,
+    gitsigns = true,
+    headlines = true,
+    illuminate = true,
+    indent_blankline = { enabled = true },
+    leap = true,
+    lsp_trouble = true,
+    mason = true,
+    markdown = true,
+    mini = true,
+    native_lsp = {
+      enabled = true,
+      underlines = {
+        errors = { 'undercurl' },
+        hints = { 'undercurl' },
+        warnings = { 'undercurl' },
+        information = { 'undercurl' },
+      },
+    },
+    navic = { enabled = true, custom_bg = 'lualine' },
+    neotest = true,
+    neotree = true,
+    noice = true,
+    notify = true,
+    semantic_tokens = true,
+    telescope = true,
+    treesitter = true,
+    treesitter_context = true,
+    which_key = true,
+  },
+}
 
 -- setup must be called before loading
 vim.cmd.colorscheme('catppuccin')

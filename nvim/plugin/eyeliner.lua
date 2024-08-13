f9be88ba1a392e64d9e2c7ac2c59a2de3e26240d
James Taranto <taranto.james@gmail.com>
2024-08-13 11:08:32 +1000

    disable eyeliner for now

--- nvim/plugin/eyeliner.lua
+++ nvim/plugin/eyeliner.lua
@@ -4,7 +4,7 @@ end
 vim.g.did_load_eyeliner_plugin = true
 
 -- Highlights unique characters for f/F and t/T motions
-require('eyeliner').setup {
-  highlight_on_key = true, -- show highlights only after key press
-  dim = true, -- dim all other characters
-}
+-- require('eyeliner').setup {
+--   highlight_on_key = true, -- show highlights only after key press
+--   dim = true, -- dim all other characters
+-- }
